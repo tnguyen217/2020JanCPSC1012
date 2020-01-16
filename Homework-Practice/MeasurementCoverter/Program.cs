56cf0cdea40c2e5e18f715fa86845a415690259a
Tu Nguyen <tnguyen217@nait.ca>
2020-01-15 17:08:30 -0700

    Update Program.cs

--- Homework-Practice/MeasurementCoverter/Program.cs
+++ Homework-Practice/MeasurementCoverter/Program.cs
@@ -15,15 +15,10 @@ namespace MeasurementCoverter
             const decimal MiCovertValue = 0.62137119m;
 
             decimal valueMi = MiCovertValue * yourKm;
-
             Console.WriteLine($"Your result from {yourKm} km to Mi is: {valueMi:0.00} mi");
 
-
-
             decimal yourMi = 1.00m;
-
             decimal valueKm = yourMi / MiCovertValue;
-
             Console.WriteLine($"Your result from {yourMi:0.00} mi to Km is: {valueKm:0.00} km");
         }
     }
