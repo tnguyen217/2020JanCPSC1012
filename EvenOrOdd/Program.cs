071add628ca225577c0f835d3456e5581325a0ab
Tu Nguyen <tnguyen217@nait.ca>
2020-01-20 18:50:21 -0700

    Even or Odd

---
+++ EvenOrOdd/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EvenOrOdd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Get Input
+            Console.Write("Enter your number: ");
+            string number = Console.ReadLine();
+            int value = int.Parse(number);
+
+            if {
+                value % 2 = 0;
+                Console.WriteLine($"Your number {value} is an even number");
+            }
+
+            else {
+                Console.WriteLine($"Your number {value} is an odd number");
+            }
+            Console.ReadLine();
+        }
+    }
+}
