e5180f6cac3a6971d60c9a17d699a6bfd4a63130
Tu Nguyen <tnguyen217@nait.ca>
2020-01-22 18:49:58 -0700

    EvenOdd Demo

---
+++ DecisionSolution/EvenOdd/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EvenOdd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number;
+            const int Two = 2;
+            string inputValue;
+
+            Console.Write("Enter your number: ");
+            inputValue = Console.ReadLine();
+            number = int.Parse(inputValue);
+
+            if ((number % Two) == 0)
+            {
+                Console.WriteLine($"Your number {number} is even"); 
+            }
+            else
+            {
+                Console.WriteLine($"Your number {number} is odd");
+            }
+        }
+    }
+}
