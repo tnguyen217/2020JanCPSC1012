01670389102269ac7167750e4c14c44742e6b21c
tnguyen217 <tnguyen217@nait.ca>
2020-03-11 18:43:58 -0600

    Random Generator demo

---
+++ ArraySolution/Question4/Program.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using Microsoft.Win32;
+
+namespace Question4
+{
+    class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            int[] driverArray = new int[25];
+            int logicalsize = ReadandLoadArray(driverArray, 25);
+            int sum = 0;
+            for (int index = 0; index < logicalsize; index++)
+            {
+                sum += driverArray[index];
+            }
+
+            if (logicalsize > 0)
+            {
+                string name = "bob";
+                Console.WriteLine($"Quiz Total: {logicalsize}");
+                Console.WriteLine($"Student     Mark");
+                Console.WriteLine($"_______     ____");
+                int index = 0;
+                while (index < logicalsize)
+                {
+                    Console.Write($"{name,-7}");
+                    Console.WriteLine($"{driverArray[index]}".PadLeft(9));
+                    index++;
+                }
+
+                FindHighandLow(driverArray, logicalsize);
+                Console.WriteLine($"The average of the {logicalsize} numbers is {(double)sum / (double)logicalsize}");
+            }
+            else
+            {
+                Console.WriteLine("You have no numbers to calculate an average.");
+            }
+        }
+
+        static int ReadandLoadArray(int[] myArray, int physicalsize)
+        {
+            //pass to your method the array reference address
+            //change it to the array within this method will alter the ORIGINAL array in the Main() method. 
+            // physicalsize is pass by Value  (a copy of the original value)
+            //any change to the physicalsize REMAINS within this method and does not affect the original value in Main()
+            int logicalsize = 0;
+
+            string Full_Path_File_Name = "";
+            OpenFileDialog fd = new OpenFileDialog();
+            fd.ShowDialog();
+            Full_Path_File_Name = fd.FileName;
+            string readValue = "";
+            StreamReader reader = null;
+
+            try
+            {
+                reader = new StreamReader(Full_Path_File_Name);
+                readValue = reader.ReadLine();
+
+                while (readValue != null && logicalsize < physicalsize)
+                {
+                    myArray[logicalsize] = int.Parse(readValue);
+                    logicalsize++;
+                    //get the next line
+                    readValue = reader.ReadLine();
+                }
+            }//eot
+            catch (Exception ex)
+            {
+                //.Message holds the error that occured during the processing of the code in your try{...}
+                Console.WriteLine($"Error: {ex.Message}");
+            }
+            finally
+            {
+                if (reader != null)
+                    reader.Close();
+            }
+
+            return logicalsize;
+        }
+
+        static void FindHighandLow(int[] myArray, int logicalsize)
+        {
+            int highest = 0;
+            int lowest = 100;
+
+            int loopcounter = 0;
+            while (loopcounter < logicalsize)
+            {
+                if (highest < myArray[loopcounter])
+                {
+                    highest = myArray[loopcounter];
+                }
+                if (lowest > myArray[loopcounter])
+                {
+                    lowest = myArray[loopcounter];
+                }
+                loopcounter++;
+            }
+            Console.WriteLine($"highest value is {highest}");
+            Console.WriteLine($"lowest value is {lowest}");
+        }
+    }
+}
---
+++ ArraySolution/RandomGen/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RandomGen
+{
+    class Program
+    {
+        //if declared outside of a method, add the keyword static infront of your declartion
+        //if declared outside of a method BUT within the class 
+        //THEN the variable is known to ALL methods WITHOUT having to pass the value as a parameter
+        //static Random rnd = new Random();
+        static void Main(string[] args)
+        {
+            //Random is a datatype of class (object)
+            //Random is just the definition of the object (describe the of object)
+            //Random is NOT a physical instance of the object
+            //to get the physical instance of the object, use the keyword new
+            //new is used by any object to create an instance of that object 
+            //to identify the object instance datatype, add the class name after the keyword new 
+            //Random is datatype 
+            //rnd is the variable name of the object instance 
+            //new is the command keyword to create an object instance 
+            //Random() is type of object instance, new is to create 
+            Random rnd = new Random();
+            int[] myArray = new int[10];
+            for(int i = 0; i < 10; i++)
+            {
+                myArray[i] = rnd.Next(1, 100);
+                Console.WriteLine($"index {i} has value of {myArray[i]}");
+            }
+
+            //What is the highest random number generated and the lowest random number generated
+            int highest = 0;
+            int lowest = 100;
+
+            int loopcounter = 0;
+            while (loopcounter < 10)
+            {
+                if (highest < myArray[loopcounter])
+                {
+                    highest = myArray[loopcounter];
+                }
+                if (lowest > myArray[loopcounter])
+                {
+                    lowest = myArray[loopcounter];
+                }
+                loopcounter++;
+            }
+            Console.WriteLine($"highest value is {highest}");
+            Console.WriteLine($"lowest value is {lowest}");
+        }
+    }
+}
